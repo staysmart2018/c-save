0004bce315555e31bc6ad68b2cc7b4ddec80a8f9
ssOfficial <88629796+staysmart2018@users.noreply.github.com>
2023-08-05 21:43:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }))
-const port = 3000
+const port = process.env.PORT || 3000
 
 mongoDB();
 
