f6f43bf4be4d2e2f3a9cc16d6ac84daf0f8b12e7
ssOfficial <88629796+staysmart2018@users.noreply.github.com>
2023-08-06 14:49:16 +0530

    Add files via upload

--- src/screens/Detail.jsx
+++ src/screens/Detail.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import BounceLoader from "react-spinners/BounceLoader";
 import { useParams } from 'react-router-dom'
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
@@ -8,10 +9,12 @@ export default function Detail() {
     const id = params.id;
 
     const [cont, setCont] = useState({});
+    const [loading, setLoading] = useState(false);
 
     const handleSubmit = async () => {
         try {
-            const response = await fetch('https://csaveserv.onrender.com/dataById', {
+            setLoading(true);
+            const response = await fetch('http://localhost:3000/dataById', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -25,8 +28,10 @@ export default function Detail() {
                 console.log('Data sent successfully');
                 const data = await response.json();
                 setCont(data[0]);
+                setLoading(false);
             } else {
                 console.log('Error sending data');
+                setLoading(false);
             }
         } catch (err) {
             console.log(err);
@@ -43,12 +48,27 @@ export default function Detail() {
             <div style={{
                 "minHeight": "100vh", "display": "flex", "flexFlow": "column"
             }}>
-                <Navbar style={{"flex":"0 1 auto"}}/>
-                <div className='container my-4' style={{"flex":"1 1 auto"}}>
-                    <h1 className='my-3'>{cont.title}</h1>
-                    <p className='my-3'><pre>{cont.content}</pre></p>
+                <Navbar style={{ "flex": "0 1 auto" }} />
+                <div className='container my-4' style={{ "flex": "1 1 auto" }}>
+                    {loading !== true
+                        ? <>
+                            <h1 className='my-3'>{cont.title}</h1>
+                            <p className='my-3'><pre>{cont.content}</pre></p>
+                        </>
+                        : <div style={{
+                            "minHeight": "100vh", "display": "flex", "justifyContent": "center", "alignItems": "center"
+                        }}
+                        >
+                            <BounceLoader
+                                className='override'
+                                size={100}
+                                color={"#36d7b7"}
+                                loading={loading} />
+                            <br />
+                        </div>
+                    }
                 </div>
-                <Footer style={{"flex":"0 1 auto"}}/>
+                <Footer style={{ "flex": "0 1 auto" }} />
             </div>
         </>
     )
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react'
+import BounceLoader from "react-spinners/BounceLoader";
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Card from '../components/Card'
 
 export default function () {
   const [cont, setCont] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const loadData = async () => {
-    let response = await fetch("https://csaveserv.onrender.com/data", {
+    setLoading(true);
+    let response = await fetch("http://localhost:3000/data", {
       method: "GET",
       headers: {
         'Content-Type': 'application/json'
@@ -16,6 +19,7 @@ export default function () {
     );
     response = await response.json();
     setCont(response);
+    setLoading(false);
   }
 
   useEffect(() => {
@@ -31,7 +35,7 @@ export default function () {
         <div className='container text-center my-4' style={{"flex":"1 1 auto"}}>
           <div className='row'>
             {
-              cont !== []
+              loading !== true
                 ? cont.map((data) => {
                   return (
                     <Card className="col-3"
@@ -41,7 +45,14 @@ export default function () {
                     />
                   )
                 })
-                : <></>
+                :<div style={{
+                  "minHeight": "100vh", "display": "flex", "justifyContent": "center", "alignItems": "center"
+                }}
+                ><BounceLoader
+                className='override' 
+                size={100} 
+                color={"#36d7b7"} 
+                loading={loading} /></div>
             }
           </div>
         </div>
