c37b9d977d82e2083203e0945bff2ea99ad090f8
ssOfficial <88629796+staysmart2018@users.noreply.github.com>
2023-08-12 23:44:00 +0530

    Update Home.jsx

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -11,7 +11,7 @@ export default function () {
 
   const loadData = async () => {
     setLoading(true);
-    let response = await fetch("http://localhost:3000/data", {
+    let response = await fetch("https://csaveserv.onrender.com/data", {
       method: "GET",
       headers: {
         'Content-Type': 'application/json'
